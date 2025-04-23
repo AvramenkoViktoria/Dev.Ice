77d9c4ae140131dc3c8eb0b244befa8b4c6345be
Viktoria Avramenko <avramenkovictoria.1402@gmail.com>
2025-04-23 18:46:03 +0300

    Initialized project structure and configured the database

---
+++ src/main/java/org/naukma/dev_ice/DevIceApplication.java
@@ -0,0 +1,12 @@
+package org.naukma.dev_ice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevIceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevIceApplication.class, args);
+    }
+}
---
+++ src/test/java/org/naukma/dev_ice/DevIceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.naukma.dev_ice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevIceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
